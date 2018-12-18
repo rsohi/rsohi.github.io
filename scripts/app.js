59baff592572d0e4d933df82b3a20c654ad486cd
rsohi999 <rsohi999@gmail.com>
2018-12-18 11:38:12 -0800

    PWA Commit v1

--- scripts/app.js
+++ scripts/app.js
@@ -15,7 +15,8 @@
 
 (function() {
   'use strict';
-
+  let deferredPrompt;
+  let btnAdd = document.getElementById("buttonAdd")
   var app = {
     isLoading: true,
     visibleCards: {},
@@ -352,4 +353,36 @@
             .register('./service-worker.js')
             .then(function() { console.log('Service Worker Registered'); });
   }
+
+  
+  //Listen to beforeinstallprompt to fire the add to desktop icon.
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    btnAdd.style.display = 'block';
+  });
+
+  btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    alert();
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+
+
+
+
 })();
